d91e8ea9891d58391cf812425ef67f1577402a9a
Mwai Kibiru <u3571680@connect.hku.hk>
2023-03-16 18:00:14 +0800

    Add files via upload

---
+++ Data Structures, Algos/A3_P1_1a.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+from typing import List
+
+import collections
+import heapq
+
+from utils import timeout
+
+
+@timeout(10)
+def mazeQ1a(graph: List[List[int]], start: List[int], end: List[int]) -> int:
+    ...
---
+++ Data Structures, Algos/A3_P1_1b.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+from typing import List
+
+import collections
+import heapq
+
+from utils import timeout
+
+
+@timeout(5)
+def mazeQ1b(graph: List[List[int]], start: List[int], end: List[int], F: int) -> int:
+    ...
